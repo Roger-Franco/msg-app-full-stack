30f8f5a179a2ec290f823335d4e2a099f581a5e5
Roger-Franco <rogerfrancogalt@gmail.com>
2022-12-03 21:10:33 -0400

    Implementa renderização de links do menu diferentes quando logado (createPost e Posts) e deslogado (Login e Register)

--- client/src/App.js
+++ client/src/App.js
@@ -50,12 +50,16 @@ function App() {
         <Router>
           <div className="navbar">
             <div className="links">
-              <Link to="/createpost">Create A Post</Link>
-              <Link to="/">Home Page</Link>
-              {!authState.status && (
+
+              {!authState.status ? (
                 <>
                   <Link to="/login">Login</Link>
                   <Link to="/registration">Registration</Link>
+                </>
+              ) : (
+                <>
+                  <Link to="/createpost">Create A Post</Link>
+                  <Link to="/">Home Page</Link>
                 </>
               )}
             </div>
